da43e1f7648f03b0f2e2103da1c2d9248cb1fddc
Taylor Lundeen <Taylor Lundeen>
2016-11-20 09:25:36 -0500

    added variables for html inputs

--- map.js
+++ map.js
@@ -4,25 +4,25 @@ var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.
 });
 var map = L.map('map', {
   center: [38.94, -77.01],
-  zoom: 11
+  zoom: 10
 });
 map.addLayer(layer);
 
 function displayPolygon(){
 
   // Get the numbers from html inputs
-  var Longitude, Latitude, Radius;
+  var Long = document.getElementsByName("Longitude");
+  var Lat = document.getElementsByName("Latitude");
+  var Rad = document.getElementsByName("Radius");
 
   // create polygon
   // Add the polygon to the map
-  var NewCircle = L.circle([Latitude, Longitude], {
+  var NewCircle = L.circle([Lat, Long], {
 	  color: '#2d862d',
 	  fillColor: '#71f471', 
-	  fillOpacity: 0.5,
-	  radius: Radius,
+	  fillOpacity: 0.3,
+	  radius: Rad,
   }).addTo(map);
   
-
- 
 }
 
